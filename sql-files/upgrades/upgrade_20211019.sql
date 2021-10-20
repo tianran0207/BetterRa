2dbc310e43551f545d039d454441754b6eabe3f6
tianran0207 <809236588@qq.com>
2021-10-21 01:38:16 +0800

    提交sql文件

---
+++ sql-files/upgrades/upgrade_20211019.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `char`
+	MODIFY `str` int(11) unsigned NOT NULL default '0',
+	MODIFY `agi` int(11) unsigned NOT NULL default '0',
+	MODIFY `vit` int(11) unsigned NOT NULL default '0',
+	MODIFY `int` int(11) unsigned NOT NULL default '0',
+	MODIFY `dex` int(11) unsigned NOT NULL default '0',
+	MODIFY `luk` int(11) unsigned NOT NULL default '0';
